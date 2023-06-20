a77594d5aec03154a3521071ba9f073778a53a6d
AdrianoEusebio <adrianoeusebio2006c1@gmail.com>
2023-06-20 19:47:09 -0400

    Criação das telas(login, cadastro)

--- app/src/main/java/com/example/brainlity/CadastroActivity.java
+++ app/src/main/java/com/example/brainlity/CadastroActivity.java
@@ -1,14 +1,74 @@
 package com.example.brainlity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.brainlity.databinding.ActivityCadastroBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class CadastroActivity extends AppCompatActivity {
+
+    private ActivityCadastroBinding binding;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_cadastro);
+        binding = ActivityCadastroBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        mAuth = FirebaseAuth.getInstance();
+
+        binding.textViewHasCount.setOnClickListener(view -> {
+            startActivity(new Intent(this, LoginActivity.class));
+        });
+
+        binding.buttonCadastrar.setOnClickListener(v -> validaDados());
+    }
+
+    private void validaDados() {
+        String email = binding.editTextCadastroEmail.getText().toString().trim();
+        String nome = binding.editTextCadastroNome.getText().toString().trim();
+        String senha = binding.editTextCadastroSenha.getText().toString().trim();
+
+        if (!nome.isEmpty()) {
+
+            if (!email.isEmpty()) {
+
+                if (!senha.isEmpty()) {
+                    binding.progressBar.setVisibility(View.VISIBLE);
+
+                    criarContaFirebase(nome, email, senha);
+
+                    } else {
+                        Toast.makeText(this, "Informe a sua senha", Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    Toast.makeText(this, "Informe seu email", Toast.LENGTH_SHORT).show();
+                }
+            } else{
+                Toast.makeText(this, "Informe seu nome", Toast.LENGTH_SHORT).show();
+            }
+    }
+
+    private void criarContaFirebase(String nome , String email, String senha){
+        mAuth.createUserWithEmailAndPassword(email,senha).addOnCompleteListener(task ->{
+            if(task.isSuccessful()){
+                finish();
+                startActivity(new Intent(this, MainActivity.class));
+
+            } else{
+                Toast.makeText(this, "Ocorreu um erro", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/brainlity/LoginActivity.java
+++ app/src/main/java/com/example/brainlity/LoginActivity.java
@@ -2,13 +2,57 @@ package com.example.brainlity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.brainlity.databinding.ActivityLoginBinding;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private ActivityLoginBinding binding;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        mAuth = FirebaseAuth.getInstance();
+
+        binding.textViewNotCount.setOnClickListener(view ->{
+            startActivity(new Intent(this, CadastroActivity.class));
+        });
+
+        binding.buttonLogin.setOnClickListener(v ->{
+            validaDados();
+        });
+    }
+
+    private void validaDados() {
+        String email = binding.editTextLoginEmail.getText().toString().trim();
+        String senha = binding.editTextLoginSenha.getText().toString().trim();
+
+        if (!email.isEmpty()) {
+            if (!senha.isEmpty()) {
+                binding.progressBarLogin.setVisibility(View.VISIBLE);
+                acessarContaFirebase(email,senha);
+                } else {
+                    Toast.makeText(this, "Informe a sua senha", Toast.LENGTH_SHORT).show();
+                }
+            } else {
+                Toast.makeText(this, "Informe seu email", Toast.LENGTH_SHORT).show();
+            }
+    }
+    public void acessarContaFirebase(String email, String senha){
+        mAuth.signInWithEmailAndPassword(email,senha).addOnCompleteListener(task ->{
+            if(task.isSuccessful()){
+                startActivity(new Intent(this, MainActivity.class));
+            }else{
+                Toast.makeText(this, "Deu merda", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
