33b72f1df503c31700abb0050c8554eb6f4a0a73
victornoronha <victorhugonoronhas@gmail.com>
2023-08-29 08:43:34 -0400

    teste

---
+++ app/src/main/java/com/example/brainlity/screens/Menu.java
@@ -0,0 +1,16 @@
+package com.example.brainlity.screens;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.brainlity.R;
+
+public class Menu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+}
