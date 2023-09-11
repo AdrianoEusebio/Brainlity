9558625af2d5e5ccd2780bfd88b6701746490b1f
AdrianoEusebio <adrianoeusebio2006c1@gmail.com>
2023-09-10 23:01:54 -0400

    merge

--- app/src/main/java/com/example/brainlity/MenuActivity.java
+++ app/src/main/java/com/example/brainlity/MenuActivity.java
@@ -1,37 +1,18 @@
 package com.example.brainlity;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.ContextCompat;
-
-import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.Window;
-import android.view.WindowManager;
 
-import com.example.brainlity.databinding.ActivityMenuBinding;
+
+
 
 public class MenuActivity extends AppCompatActivity {
-
-    private ActivityMenuBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityMenuBinding.inflate(getLayoutInflater());
+        setContentView(R.layout.activity_menu);
 
-        setContentView(binding.getRoot());
-
-       if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            Window window = getWindow();
-            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-            window.setStatusBarColor(ContextCompat.getColor(this, R.color.cor_principal));
-
-            binding.configuracao.setOnClickListener(view ->{
-                startActivity(new Intent(this, ConfiguracaoActivity.class));
-            });
-        }
 
     }
 }
