97107e4f433613d56ee749ed442bde36acefb263
AdrianoEusebio <adrianoeusebio2006c1@gmail.com>
2023-10-29 15:37:45 -0400

    Modificações(Ansiedade e Foco completa, conhecimento em progresso)

--- app/src/main/java/com/example/brainlity/Exercicios/FocoFragment.java
+++ app/src/main/java/com/example/brainlity/Exercicios/FocoFragment.java
@@ -1,5 +1,8 @@
 package com.example.brainlity.Exercicios;
 
+import android.content.Intent;
+import android.graphics.Paint;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,60 +10,34 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.brainlity.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link FocoFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import org.w3c.dom.Text;
+
 public class FocoFragment extends Fragment {
+    private View view;
+    private TextView textView;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public FocoFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment FocoFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static FocoFragment newInstance(String param1, String param2) {
-        FocoFragment fragment = new FocoFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_foco, container, false);
+        view = inflater.inflate(R.layout.fragment_foco, container, false);
+        textView = view.findViewById(R.id.text_link3);
+        textView.setPaintFlags(textView.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
+        textView.setOnClickListener(v -> {
+            String url = "https://www.unimedcampinas.com.br/blog/saude-emocional/o-que-causa-a-dificuldade-de-concentracao-e-o-que-ela-pode-significar";
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+            startActivity(intent);
+        });
+        return view;
     }
 }
