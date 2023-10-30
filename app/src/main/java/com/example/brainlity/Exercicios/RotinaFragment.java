4084d3be593c9670073800b60a06445c2aecf22b
AdrianoEusebio <adrianoeusebio2006c1@gmail.com>
2023-10-30 18:29:13 -0400

    Modificações(saude mental completa, conhecimento em progresso)

--- app/src/main/java/com/example/brainlity/Exercicios/RotinaFragment.java
+++ app/src/main/java/com/example/brainlity/Exercicios/RotinaFragment.java
@@ -1,5 +1,8 @@
 package com.example.brainlity.Exercicios;
 
+import android.content.Intent;
+import android.graphics.Paint;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,60 +10,31 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.brainlity.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link RotinaFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class RotinaFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public RotinaFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment RotinaFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static RotinaFragment newInstance(String param1, String param2) {
-        RotinaFragment fragment = new RotinaFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
+    View view;
+    TextView textView;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_rotina, container, false);
+        view = inflater.inflate(R.layout.fragment_rotina, container, false);
+        textView = view.findViewById(R.id.text_link4);
+        textView.setPaintFlags(textView.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
+        textView.setOnClickListener(v -> {
+            String url = "https://www.conexasaude.com.br/blog/rotina-produtiva/";
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+            startActivity(intent);
+        });
+        return view;
     }
 }
