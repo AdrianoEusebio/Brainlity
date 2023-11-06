0b4a790a0fa70de79c40fb4b78e154a49a82e498
AdrianoEusebio <adrianoeusebio2006c1@gmail.com>
2023-11-05 22:57:55 -0400

    Correções

--- app/src/main/java/com/example/brainlity/PerfilFragment.java
+++ app/src/main/java/com/example/brainlity/PerfilFragment.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
@@ -21,7 +22,10 @@ import com.example.brainlity.Activity.LoginActivity;
 import com.example.brainlity.Activity.MenuActivity;
 import com.example.brainlity.Utils.AsyncTask;
 import com.example.brainlity.Utils.Standard;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
@@ -29,8 +33,9 @@ import com.google.firebase.firestore.FirebaseFirestore;
 public class PerfilFragment extends Fragment {
 
     private View view;
+    private FirebaseAuth mAuth = FirebaseAuth.getInstance();
     private Boolean passwordVisible = false;
-    private TextView textSenha, textEmail, textNome, exit, alterarSenha;
+    private TextView textSenha, textEmail, textNome, exit, textExit2;
     private ImageView imageView,imageView2;
     private Standard standard = new Standard();
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
@@ -50,6 +55,7 @@ public class PerfilFragment extends Fragment {
         asyncTask.execute();
         usersCollection = db.getInstance().collection("Users");
         view = inflater.inflate(R.layout.fragment_perfil, container, false);
+        textExit2 = view.findViewById(R.id.textView_exit2);
         textNome = view.findViewById(R.id.textView_userNome);
         imageView2 = view.findViewById(R.id.imageView2);
         textSenha = view.findViewById(R.id.textView_userSenha);
@@ -72,7 +78,9 @@ public class PerfilFragment extends Fragment {
             openPopup();
 
         });
+        textSenhaClick();
         return view;
+
     }
 
     public void togglePasswordVisibility() {
@@ -85,6 +93,44 @@ public class PerfilFragment extends Fragment {
 
                 textSenha.setInputType(android.text.InputType.TYPE_CLASS_TEXT | android.text.InputType.TYPE_TEXT_VARIATION_PASSWORD);
                 imageView2.setImageResource(R.drawable.baseline_visibility);
+            }
+        });
+    }
+
+
+    public void textSenhaClick(){
+        textExit2.setOnClickListener(v -> {
+            AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(getContext());
+            dialogBuilder.setTitle("Verificação de email");
+            EditText editTextName = new EditText(getContext());
+            dialogBuilder.setView(editTextName);
+            dialogBuilder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    String email = editTextName.getText().toString();
+                    recuperarSenha(email);
+                }
+            });
+
+            dialogBuilder.setNegativeButton("cancelar", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+
+                }
+            });
+            AlertDialog dialog = dialogBuilder.create();
+            dialog.show();
+        });
+    }
+    public void recuperarSenha(String email){
+        mAuth.sendPasswordResetEmail(email).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if(task.isSuccessful()){
+                    standard.toast((AppCompatActivity) getActivity(),"Email Enviado com sucesso",1);
+                } else {
+                    standard.toast((AppCompatActivity) getActivity(),"Falha ao enviar o email",2);
+                }
             }
         });
     }
